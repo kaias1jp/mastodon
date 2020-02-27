a12aba430d520957fa8e7f2ef98d321c99a0d48b
noellabo <noel.yoshiba@gmail.com>
2020-02-28 07:45:54 +0900

    Fix to reject status quotations other than public and unlisted

--- app/javascript/mastodon/features/video/index.js
+++ app/javascript/mastodon/features/video/index.js
@@ -121,6 +121,8 @@ class Video extends React.PureComponent {
     autoPlay: PropTypes.bool,
     volume: PropTypes.number,
     muted: PropTypes.bool,
+    link: PropTypes.node,
+    quote: PropTypes.bool,
   };
 
   static defaultProps = {
@@ -533,7 +535,7 @@ class Video extends React.PureComponent {
   }
 
   render () {
-    const { preview, src, inline, onOpenVideo, onCloseVideo, intl, alt, detailed, sensitive, editable, blurhash } = this.props;
+    const { preview, src, inline, onOpenVideo, onCloseVideo, intl, alt, detailed, sensitive, editable, blurhash, link, quote } = this.props;
     const { containerWidth, currentTime, duration, volume, buffer, dragging, paused, fullscreen, hovered, muted, revealed } = this.state;
     const progress = Math.min((currentTime / duration) * 100, 100);
     const playerStyle = {};
@@ -544,6 +546,11 @@ class Video extends React.PureComponent {
       width  = containerWidth;
       height = containerWidth / (16/9);
 
+      playerStyle.height = height;
+    }
+
+    if (quote && height) {
+      height /= 2;
       playerStyle.height = height;
     }
 
