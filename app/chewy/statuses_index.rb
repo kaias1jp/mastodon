69495fa151f08231a229d53ad4ef07df3144478c
kaias1jp <kaias1jp@gmail.com>
2020-09-24 12:47:19 +0900

    enable kuromoji 	modified:   app/chewy/statuses_index.rb

--- app/chewy/statuses_index.rb
+++ app/chewy/statuses_index.rb
@@ -1,35 +1,25 @@
 # frozen_string_literal: true
 
 class StatusesIndex < Chewy::Index
-  settings index: { refresh_interval: '15m' }, analysis: {
-    filter: {
-      english_stop: {
-        type: 'stop',
-        stopwords: '_english_',
-      },
-      english_stemmer: {
-        type: 'stemmer',
-        language: 'english',
-      },
-      english_possessive_stemmer: {
-        type: 'stemmer',
-        language: 'possessive_english',
-      },
-    },
-    analyzer: {
-      content: {
-        tokenizer: 'uax_url_email',
-        filter: %w(
-          english_possessive_stemmer
-          lowercase
-          asciifolding
-          cjk_width
-          english_stop
-          english_stemmer
-        ),
-      },
-    },
-  }
+    settings index: { refresh_interval: '15m' }, analysis: {
+          tokenizer: {
+                  kuromoji_user_dict: {
+                            type: 'kuromoji_tokenizer',
+                                  },
+                                      },
+                                          analyzer: {
+                                              content: {
+                                                        type: 'custom',
+                                                                tokenizer: 'kuromoji_user_dict',
+                                                                        filter: %w(
+                                                                                  kuromoji_baseform
+                                                                                            kuromoji_stemmer
+                                                                                                      cjk_width
+                                                                                                                lowercase
+                                                                                                                        ),
+                                                                                                                              },
+                                                                                                                                  },
+                                                                                                                                    }
 
   define_type ::Status.unscoped.kept.without_reblogs.includes(:media_attachments), delete_if: ->(status) { status.searchable_by.empty? } do
     crutch :mentions do |collection|
